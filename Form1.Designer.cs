1223ac89462d2a9f95393b8138da42b414b4c9af
Kazi Safayat Huq <65438156+KaziHuq@users.noreply.github.com>
2021-04-27 14:49:49 -0400

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,170 @@
+﻿
+namespace MyFormTwo
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.lbl1 = new System.Windows.Forms.Label();
+            this.lbl2 = new System.Windows.Forms.Label();
+            this.txtBox1 = new System.Windows.Forms.TextBox();
+            this.txtBox2 = new System.Windows.Forms.TextBox();
+            this.btn1 = new System.Windows.Forms.Button();
+            this.btn2 = new System.Windows.Forms.Button();
+            this.btn3 = new System.Windows.Forms.Button();
+            this.lbl3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label1.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label1.Location = new System.Drawing.Point(387, 41);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(313, 48);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Simple Calculator";
+            // 
+            // lbl1
+            // 
+            this.lbl1.AutoSize = true;
+            this.lbl1.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.lbl1.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.lbl1.Location = new System.Drawing.Point(346, 150);
+            this.lbl1.Name = "lbl1";
+            this.lbl1.Size = new System.Drawing.Size(121, 32);
+            this.lbl1.TabIndex = 1;
+            this.lbl1.Text = "FirstValue:";
+            // 
+            // lbl2
+            // 
+            this.lbl2.AutoSize = true;
+            this.lbl2.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.lbl2.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.lbl2.Location = new System.Drawing.Point(346, 214);
+            this.lbl2.Name = "lbl2";
+            this.lbl2.Size = new System.Drawing.Size(156, 32);
+            this.lbl2.TabIndex = 2;
+            this.lbl2.Text = "SecondValue:";
+            // 
+            // txtBox1
+            // 
+            this.txtBox1.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.txtBox1.Location = new System.Drawing.Point(519, 147);
+            this.txtBox1.Name = "txtBox1";
+            this.txtBox1.Size = new System.Drawing.Size(212, 39);
+            this.txtBox1.TabIndex = 3;
+            // 
+            // txtBox2
+            // 
+            this.txtBox2.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.txtBox2.Location = new System.Drawing.Point(519, 214);
+            this.txtBox2.Name = "txtBox2";
+            this.txtBox2.Size = new System.Drawing.Size(212, 39);
+            this.txtBox2.TabIndex = 4;
+            // 
+            // btn1
+            // 
+            this.btn1.Font = new System.Drawing.Font("Segoe UI", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.btn1.Location = new System.Drawing.Point(346, 303);
+            this.btn1.Name = "btn1";
+            this.btn1.Size = new System.Drawing.Size(175, 48);
+            this.btn1.TabIndex = 5;
+            this.btn1.Text = "Add";
+            this.btn1.UseVisualStyleBackColor = true;
+            this.btn1.Click += new System.EventHandler(this.btn1_Click);
+            // 
+            // btn2
+            // 
+            this.btn2.Font = new System.Drawing.Font("Segoe UI", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.btn2.Location = new System.Drawing.Point(556, 303);
+            this.btn2.Name = "btn2";
+            this.btn2.Size = new System.Drawing.Size(175, 48);
+            this.btn2.TabIndex = 6;
+            this.btn2.Text = "Multiply";
+            this.btn2.UseVisualStyleBackColor = true;
+            this.btn2.Click += new System.EventHandler(this.btn2_Click);
+            // 
+            // btn3
+            // 
+            this.btn3.Font = new System.Drawing.Font("Segoe UI", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.btn3.Location = new System.Drawing.Point(442, 383);
+            this.btn3.Name = "btn3";
+            this.btn3.Size = new System.Drawing.Size(175, 48);
+            this.btn3.TabIndex = 7;
+            this.btn3.Text = "Reset";
+            this.btn3.UseVisualStyleBackColor = true;
+            this.btn3.Click += new System.EventHandler(this.btn3_Click);
+            // 
+            // lbl3
+            // 
+            this.lbl3.AutoSize = true;
+            this.lbl3.Font = new System.Drawing.Font("Segoe UI", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.lbl3.ForeColor = System.Drawing.Color.Yellow;
+            this.lbl3.Location = new System.Drawing.Point(155, 506);
+            this.lbl3.Name = "lbl3";
+            this.lbl3.Size = new System.Drawing.Size(0, 38);
+            this.lbl3.TabIndex = 8;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.ClientSize = new System.Drawing.Size(1064, 565);
+            this.Controls.Add(this.lbl3);
+            this.Controls.Add(this.btn3);
+            this.Controls.Add(this.btn2);
+            this.Controls.Add(this.btn1);
+            this.Controls.Add(this.txtBox2);
+            this.Controls.Add(this.txtBox1);
+            this.Controls.Add(this.lbl2);
+            this.Controls.Add(this.lbl1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "CalculatorApp";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lbl1;
+        private System.Windows.Forms.Label lbl2;
+        private System.Windows.Forms.TextBox txtBox1;
+        private System.Windows.Forms.TextBox txtBox2;
+        private System.Windows.Forms.Button btn1;
+        private System.Windows.Forms.Button btn2;
+        private System.Windows.Forms.Button btn3;
+        private System.Windows.Forms.Label lbl3;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MyFormTwo
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+private void btn1_Click(object sender, EventArgs e)
+        {
+            double txt1 = Convert.ToDouble(txtBox1.Text);
+            double txt2 = Convert.ToDouble(txtBox2.Text);
+
+            if (txt1 < 0 || txt2 < 0)
+            {
+                lbl3.ForeColor = Color.Red;
+                lbl3.Text = "Value must be numeric and > 0";
+            }
+
+            else
+            {
+
+                double sum = txt1 + txt2;
+
+                lbl3.Text = sum.ToString();
+                
+            }
+            
+        }
+
+        private void btn2_Click(object sender, EventArgs e)
+        {
+            double txt1 = Convert.ToDouble(txtBox1.Text);
+            double txt2 = Convert.ToDouble(txtBox2.Text);
+
+            if (txt1 < 0 || txt2 < 0)
+            {
+                lbl3.ForeColor = Color.Red;
+                lbl3.Text = "Value must be numeric and > 0";
+            }
+
+            else
+            {
+
+                double sum = txt1 * txt2;
+                lbl3.ForeColor = Color.Yellow;
+                lbl3.Text = sum.ToString();
+
+            }
+        }
+
+        private void btn3_Click(object sender, EventArgs e)
+        {
+            txtBox1.Clear();
+            txtBox2.Clear();
+            lbl3.Text = " ";
+        }
+    }
+}
